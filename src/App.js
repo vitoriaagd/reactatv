b7c9bb16570e51dc992e3a76ca7b732f4e6d7a1b
VITORIA GONCALVES DE DEUS <04103507020@senacrs.edu.br>
2024-07-02 11:25:54 -0300

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TodoList from "./Componentes/toDoList";
+ 
 function App() {
+  const tasksList = [
+    { id: 1, text: 'Aprender React' },
+    { id: 2, text: 'Construir um projeto simples' },
+    { id: 3, text: 'Entender Props e State' }
+  ];
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoList content={tasksList} />
   );
 }
-
+ 
 export default App;
